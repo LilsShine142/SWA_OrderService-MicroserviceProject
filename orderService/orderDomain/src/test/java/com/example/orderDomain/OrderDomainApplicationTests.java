adecbed6248f3e8d03e34d3388c65a426f770bfc
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2025-10-24 10:02:53 +0700

    setup order service project

---
+++ orderService/orderDomain/src/test/java/com/example/orderDomain/OrderDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderDomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
